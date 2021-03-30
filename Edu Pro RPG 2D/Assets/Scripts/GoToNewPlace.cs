430a9a7bd6c2abade5865f840e3d840af48d4072
theWandroid <thewandreid@gmail.com>
2021-03-30 12:45:55 +0200

    first commit

---
+++ Edu Pro RPG 2D/Assets/Scripts/GoToNewPlace.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNewPlace : MonoBehaviour
+{
+    //Nombre de la escena a la que voy a teletransportar al jugador
+    public string newPlaceName;
+    public string uuid;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.gameObject.tag == "Player")
+        {
+            FindObjectOfType<PlayerController>().nextUuid = uuid;
+            Debug.Log(FindObjectOfType<PlayerController>().nextUuid);
+            SceneManager.LoadScene(newPlaceName);
+        }
+    }
+
+}
---
+++ Edu Pro RPG 2D/Assets/Scripts/StartPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPoint : MonoBehaviour
+{
+
+    private PlayerController player;
+    private CameraFollow theCamera;
+    public Vector2 facingDirection = Vector2.zero;
+
+    //nombre del identificador del Start Point
+    public string uuid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        theCamera = FindObjectOfType<CameraFollow>();
+
+        if (player.nextUuid == null|| !player.nextUuid.Equals(uuid))
+        {
+            return;
+        }
+        player.transform.position = this.transform.position;
+        theCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, theCamera.transform.position.z);
+
+        player.lastMovement = facingDirection;
+        
+    }
+}
