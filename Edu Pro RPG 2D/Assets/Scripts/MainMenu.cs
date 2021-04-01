4254b9e7f6f48c98fc24afaa3a73e09b8224a2b6
krispisag <krispisag@gmail.com>
2021-04-01 13:16:41 +0200

    13:16

--- Edu Pro RPG 2D/Assets/Scripts/MainMenu.cs
+++ Edu Pro RPG 2D/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public string firstLevel;
 public void StartGame()
     {
-
+        SceneManager.LoadScene(firstLevel);
     }
 
 public void OpenOptions()
