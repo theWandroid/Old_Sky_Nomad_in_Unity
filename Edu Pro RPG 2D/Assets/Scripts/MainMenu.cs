f9de7e66d50a468fa3e603080f8f3cd7abee9c45
theWandroid <thewandreid@gmail.com>
2021-04-02 13:44:14 +0200

    tyhtfghfggh
    update

---
+++ Edu Pro RPG 2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string firstLevel;
+public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+public void OpenOptions()
+    { 
+    
+    }
+
+public void CloseOptions()
+    {
+
+    }
+
+public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
