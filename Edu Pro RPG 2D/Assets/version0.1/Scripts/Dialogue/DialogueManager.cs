41a6167dce66f161e463120ef928834f061b4cfe
theWandroid <thewandreid@gmail.com>
2021-04-09 12:48:38 +0200

    update 0.1v

---
+++ Edu Pro RPG 2D/Assets/version0.1/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public TextMeshProUGUI dialogueText;
+    public Image avatarImage;
+    public bool dialogueActive;
+
+    public string[] dialogueLines;
+    public int currentDialogueLine;
+
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        dialogueActive = false;
+        dialogueBox.SetActive(false);
+        //localizo al player por su variable
+        playerController = FindObjectOfType<PlayerController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            //cada vez que pulsamos espacio se pasa a la siguiente linea de dialogo
+            currentDialogueLine++;
+            /*
+            dialogueActive = false;
+            avatarImage.enabled = false;
+            dialogueBox.SetActive(false);
+            */
+        
+            //si la linea actual es mayor o igual que las lineas de dialogo
+            if (currentDialogueLine>= dialogueLines.Length)
+            {
+                currentDialogueLine = 0;
+                dialogueActive = false;
+                avatarImage.enabled = false;
+                dialogueBox.SetActive(false);
+                playerController.isTalking = false;
+            }
+            else
+            {
+                dialogueText.text = dialogueLines[currentDialogueLine];
+            }
+        }
+
+    }
+
+    public void ShowDialogue(string [] lines)
+    {
+        currentDialogueLine = 0;
+        dialogueLines = lines;
+        dialogueActive = true;
+        dialogueBox.SetActive(true);
+        //dialogueText.text = text;
+        dialogueText.text = dialogueLines[currentDialogueLine];
+        playerController.isTalking = true;
+    }
+
+    public void ShowDialogue(string [] lines, Sprite sprite)
+    {
+        ShowDialogue(lines);
+        avatarImage.enabled = true;
+        avatarImage.sprite = sprite;
+    }
+}
---
+++ Edu Pro RPG 2D/Assets/version0.1/Scripts/Pixel Adventure/CheckGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    public static bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+}
