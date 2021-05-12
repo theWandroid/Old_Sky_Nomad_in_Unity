12fee430d32cce1f51b846ca1b8d5e8940272061
Raulsbd <75973350+Raulsbd@users.noreply.github.com>
2021-05-12 14:45:48 +0200

    up

---
+++ Edu Pro RPG 2D/Assets/version0.1/_Group Members/Raul/Cambiacapa.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cambiacapa : MonoBehaviour
+{
+     public GameObject Capa;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+       
+    }
+}
