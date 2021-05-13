f3f545241371308ab2197d6c436859fe8afc604e
Cristina <you@example.com>
2021-05-13 12:30:05 +0200

    Firebase #1

---
+++ Edu Pro RPG 2D/Assets/Firebase/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeScene(int _sceneIndex)
+    {
+        SceneManager.LoadSceneAsync(_sceneIndex);
+    }
+}
