803f8ca24f07fd8a665e54dc4a9bf88888e98fc6
theWandroid <thewandreid@gmail.com>
2021-05-14 08:02:07 +0200

    implementando joystick

---
+++ Edu Pro RPG 2D/Assets/version0.1/_Group Members/Andrei/Scripts/JoyButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    [HideInInspector]
+    protected bool Pressed;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+}
