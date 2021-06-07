38203481a9b864060228716bb3589b70be842b77
theWandroid <thewandreid@gmail.com>
2021-06-07 02:26:53 +0200

    joystick

---
+++ Edu Pro RPG 2D/Assets/version0.1/Scripts/Camera/CameraPlatform.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlatform : MonoBehaviour
+{
+    [Tooltip("Here goes the GameObject we want to follow with the Camera")]
+    public GameObject target;
+    [Tooltip("Here goes the position of the GameObject that we have described before")]
+    private Vector3 targetPosition;
+    [Tooltip("This is a variable for the speed of the camera")]
+    public float cameraSpeed;
+    private Camera theCamera;
+    private Vector3 minLimits, maxLimits;
+    private float halfHeight, halfWidth;
+    // Start is called before the first frame update
+  
+    public void ChangeLimits(BoxCollider2D cameraLimits)
+    {
+
+        //cameraLimits = GameObject.Find("Limits").GetComponent<BoxCollider2D>();
+
+        minLimits = cameraLimits.bounds.min;
+        maxLimits = cameraLimits.bounds.max;
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = (halfHeight / Screen.height) * Screen.width;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Mathf.Clamp(this.target.transform.position.x, minLimits.x + halfWidth, maxLimits.x - halfWidth);
+        float posY = Mathf.Clamp(this.target.transform.position.y, minLimits.y + halfWidth, maxLimits.y - halfWidth);
+
+        //actualizra la posicion del personaje en todo moento
+        targetPosition = new Vector3(posX, posY, this.transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * cameraSpeed);
+    }
+
+}
